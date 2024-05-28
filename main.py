7c298c83ccf9b554faa05c21ed073337e36c81c2
Finkch <skyler@thealdersons.org>
2024-05-28 15:43:42 -0700

    Added basic main to play around with

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 # A little project to turn images and gifs into pixel art
 
+from input import *
+from image import Image
+
 def main():
-    pass
+
+    process = True
+
+    while process:
+
+        image = Image()
+        choose_file(image)
+        choose_palette_size(image)
+        choose_resolution(image)
+
+        pim = image.image
+        print(pim.format, pim.size, pim.mode)
+
+        process = choose_continue()
 
 main()
