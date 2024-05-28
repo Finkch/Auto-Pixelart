e725e704920d73bdbc13728987399e88f385ec9e
Finkch <skyler@thealdersons.org>
2024-05-28 14:48:41 -0700

    Made function to prompt user, refactored choose_file()

--- input.py
+++ input.py
@@ -8,22 +8,12 @@ def choose_file():
     # Obtains a list of files in the input directory
     files = listdir('PUT IMAGES HERE')
 
-    # Gets a list of valid user inputs
-    valid_choices = [str(i + 1) for i in range(len(files))]
-
-    # Gets faulty default value to begin while loop
-    choice = 'n/a'
-
-
-    # Gets a valid choice from the user
-    while choice not in valid_choices:
-
-        # Prompts the user
-        print('Please chooe file to turn into pixel art:')        
-        [print(f'[{i + 1}]\t{files[i]}') for i in range(len(files))]
-
-        # Obtains the choice
-        choice = input('% ')
+    # Obtains user's choice
+    choice = prompt(
+        preamble        = 'Please chooe file to turn into pixel art:',
+        valid_choices   = [str(i + 1) for i in range(len(files))],
+        prompts         = [f'[{i + 1}]\t{files[i]}' for i in range(len(files))]
+        )
 
     # Returns the file choses
     return files[int(choice) - 1]
@@ -42,3 +32,22 @@ def choose_resolution():
 # Prompts user for whether to process another image
 def choose_continue():
     pass
+
+
+# Prompts user, repeating prompt until given a valid input
+def prompt(preamble, valid_choices, prompts):
+    
+    # Defaults choice to be invalid
+    choice = None
+
+    # Loops until given valid choice
+    while choice not in valid_choices:
+
+        # Prompts the user
+        print(f'\n{preamble}')
+        [print(prompt) for prompt in prompts]
+
+        # Obtains user input
+        choice = input('% ')
+    
+    return choice
