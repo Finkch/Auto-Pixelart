c81462ba1e964a98a11af5aa7a13d1985d000933
Finkch <skyler@thealdersons.org>
2024-05-28 14:31:02 -0700

    Combined functions such that  is a single function

--- read.py
+++ read.py
@@ -2,13 +2,28 @@
 
 from os import listdir
 
-# Returns a list of images present in the input directory
-def report_images():
+# Obtains user's choice for file to process
+def choose_file():
 
-    # Gets a list of files
+    # Obtains a list of files in the input directory
     files = listdir('PUT IMAGES HERE')
-    
-    # Formats the list into a presentable string
-    out = [f'[{i + 1}]\t{files[i]}' for i in range(len(files))]
 
-    return out
+    # Gets a list of valid user inputs
+    valid_choices = [str(i + 1) for i in range(len(files))]
+
+    # Gets faulty default value to begin while loop
+    choice = 'n/a'
+
+
+    # Gets a valid choice from the user
+    while choice not in valid_choices:
+
+        # Prompts the user
+        print('Please chooe file to turn into pixel art:')        
+        [print(f'[{i + 1}]\t{files[i]}') for i in range(len(files))]
+
+        # Obtains the choice
+        choice = input('% ')
+
+    # Returns the file choses
+    return files[int(choice) - 1]
