d39aba668ead90ba4c92e91faa0d67c6cd8a798d
Finkch <skyler@thealdersons.org>
2024-05-28 16:01:53 -0700

    Added skeleton for a colour class

---
+++ colour.py
@@ -0,0 +1,10 @@
+# Tracks colour and colour operations
+
+from image import Image
+
+class Colour:
+    def __init__(self, R: int = 0, G: int = 0, B: int = 0):
+        self.R = R
+        self.G = G
+        self.B = B
+
