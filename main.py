fe27723e99b65d695c6eeb027001c3d136004c2f
Finkch <skyler@thealdersons.org>
2024-05-29 12:19:07 -0700

    More performance trial stuff in prep of a branch change

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from time import process_time as clock
 
 def main():
 
+    print('Program start!')
+
     loc = 'PUT IMAGES HERE/'
 
     process = True
@@ -30,14 +32,17 @@ def main():
     image = Image()
     choose_file(image)
 
-    # trials = 10
-    # threads = [1, 2, 4, 8, 16, 32]
-
     trials = 10
-    threads = [2]
+    threads = [1, 2, 4, 8, 16, 32]
 
-    ptimes = []
+    # trials = 10
+    # threads = [2]
+
+    ttimes = []
     stimes = []
+    ptimes = []
+
+    print_individual = False
 
 
     for t in threads:
@@ -46,21 +51,39 @@ def main():
             scounts = count_colours_serial(image)
             end = clock()
             stimes.append(end - start)
+
+            if print_individual:
+                print(f'Serial trial {i}:\t{stimes[-1]}')
             
             
+    # for t in threads:
+    #     for i in range(trials):
+    #         start = clock()
+    #         tcounts = count_colours_two(image)
+    #         end = clock()
+    #         ttimes.append(end - start)
+
+    #         if print_individual:
+    #           print(f'Thread trial {i}:\t{ttimes[-1]}')
+
     for t in threads:
         for i in range(trials):
             start = clock()
-            tcounts = count_colours_two(image)
+            pcounts = count_colours_multiprocess(image, t)
             end = clock()
             ptimes.append(end - start)
 
+            if print_individual:
+                print(f'Multiprocess trial {i}:\t{ptimes[-1]}')
+
+
         print(f'Number of threads: {t}')
-        print(f'Parallel time:\t{sum(ptimes) / len(ptimes)} s')
+        # print(f'Thread time:\t{sum(ttimes) / len(ttimes)} s')
         print(f'Serial time:\t{sum(stimes) / len(stimes)} s')
-        print()
+        print(f'Multiprocess time:\t{sum(ptimes) / len(ptimes)} s')
 
 
-    print(tcounts == scounts)
+    # print(tcounts == scounts)
     
-main()
+if __name__ == '__main__':
+    main()
