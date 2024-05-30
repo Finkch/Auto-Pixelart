ef01cce33f1af20a93faccf344958e9029806136
Finkch <skyler@thealdersons.org>
2024-05-30 15:50:42 -0700

    Added weighted_colour() to average set of colours but with weights of their occurances

--- colour.py
+++ colour.py
@@ -1,13 +1,26 @@
 # Tracks colour and colour operations
+from numpy import average
 
 # Returns the average colour in a set of colours
 def average_colour(colours):
     return (
-        int(sum(colour[0] for colour in colours) / len(colours)),
+        int(average([colour[0] for colour in colours])),
         (
-            int(sum([colour[1][0] for colour in colours]) / len(colours)), 
-            int(sum([colour[1][1] for colour in colours]) / len(colours)), 
-            int(sum([colour[1][2] for colour in colours]) / len(colours)),
+            int(average([colour[1][0] for colour in colours])), 
+            int(average([colour[1][1] for colour in colours])), 
+            int(average([colour[1][2] for colour in colours])),
+        )
+    )
+
+# Same as average_colour(), but where the weights are the occurances
+def weighted_colour(colours):
+    weights = [colour[0] for colour in colours]
+    return (
+        int(average([colour[0] for colour in colours])),
+        (
+            int(average([colour[1][0] for colour in colours], weights = weights)), 
+            int(average([colour[1][1] for colour in colours], weights = weights)), 
+            int(average([colour[1][2] for colour in colours], weights = weights)),
         )
     )
 
