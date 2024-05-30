3728f20b5470acadc7e712533d26c516b6d93e37
Finkch <skyler@thealdersons.org>
2024-05-30 15:39:48 -0700

    Added functions to aggregate a set of colours

--- colour.py
+++ colour.py
@@ -1,7 +1,17 @@
 # Tracks colour and colour operations
 
-class Colour:
-    def __init__(self, R: int = 0, G: int = 0, B: int = 0):
-        self.R = R
-        self.G = G
-        self.B = B
+# Returns the average colour in a set of colours
+def average_colour(colours):
+    return (
+        int(sum(colour[0] for colour in colours) / len(colours)),
+        (
+            int(sum([colour[1][0] for colour in colours]) / len(colours)), 
+            int(sum([colour[1][1] for colour in colours]) / len(colours)), 
+            int(sum([colour[1][2] for colour in colours]) / len(colours)),
+        )
+    )
+
+# Returns the most common colour in a set
+def common_colour(colours):
+    return max(colours, key = lambda x : x[0])
+
