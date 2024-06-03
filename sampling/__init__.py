489da8e1eee71788e57c5f267e15113bed7eb387
Finkch <skyler@thealdersons.org>
2024-06-03 10:58:54 -0700

    Added new directory to contain downscaling function

---
+++ sampling/__init__.py
@@ -0,0 +1,1 @@
+# Empty init so this is recognised as a library
---
+++ sampling/nearest_neighbour.py
@@ -0,0 +1,9 @@
+# A simple sampling algorithm: nearest neighbours
+#   Maps the downscaled image to the original image,
+#   taking each new pixel to be the closest pixel to
+#   that point in the original.
+
+from image import Image
+
+def nearest_neighbour(image: Image, pixel_art: Image) -> Image:
+    pass
