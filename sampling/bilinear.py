69ed6de0237071a5fd26fe4932784108da25406b
Finkch <skyler@thealdersons.org>
2024-06-03 16:49:02 -0700

    Updated bilinear sampling to reflect Colour changes

--- sampling/bilinear.py
+++ sampling/bilinear.py
@@ -3,6 +3,7 @@
 #   First two over the x, then one over the y.
 
 from image import Image
+from colour import Colour
 
 from colour import weighted_colour
 
@@ -39,14 +40,14 @@ def bilinear(image: Image, pixel_art: Image, log: bool = False) -> Image:
             sjr = sjf + 1
 
             # Averages over the two x's
-            x1 = weighted_colour([source_map[sif, sjf], source_map[sir, sjf]], [sir - si, si - sif])
-            x2 = weighted_colour([source_map[sif, sjr], source_map[sir, sjr]], [sir - si, si - sif])
+            x1 = weighted_colour([Colour(source_map[sif, sjf]), Colour(source_map[sir, sjf])], [sir - si, si - sif])
+            x2 = weighted_colour([Colour(source_map[sif, sjr]), Colour(source_map[sir, sjr])], [sir - si, si - sif])
 
             # Average over the averages, i.e. average in the y
             y = weighted_colour([x1, x2], [sjr - sj, sj - sjf])
 
             # Paints the colour
-            pixel_map[i, j] = y
+            pixel_map[i, j] = y.RGB
 
 
             if log:
