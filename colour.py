de118a4fcc45c878c8c3810371a9520360ebfe96
Finkch <skyler@thealdersons.org>
2024-06-12 11:22:08 -0700

    Colour can now use HSV

--- colour.py
+++ colour.py
@@ -6,7 +6,7 @@ from logger import logger
 
 # Class to represent RGB colour
 class Colour:
-    def __init__(self, R: int, G: int, B: int, frequency: int = 1) -> None:
+    def __init__(self, R: int, G: int, B: int, frequency: int = 1, use_HSV: bool = False) -> None:
 
         # Grabs the channels
         self.R = R
@@ -14,6 +14,8 @@ class Colour:
         self.B = B
 
         self.frequency = frequency
+
+        self.use_HSV = use_HSV
         
         # # Enforces each channel to be an int and within an appropriate range
         self.R = max(0, min(255, int(self.R)))
@@ -23,9 +25,23 @@ class Colour:
 
         # Packages the three channels together
         self.RGB = (self.R, self.G, self.B)
+
+        self.HSV = None
+
+        # Gets HSV for self
+        if use_HSV:
+            self.get_HSV()
+
+    # Calling the colour returns its RGB or HSV
+    def __call__(self) -> tuple[float]:
+        if self.use_HSV:
+            return self.HSV
+        return self.RGB
     
     # tostring
     def __str__(self):
+        if self.use_HSV:
+            return f'({self.HSV}, {self.frequency})'
         return f'({self.RGB}, {self.frequency})'
     
     # Addition
@@ -87,6 +103,53 @@ class Colour:
             self.B / other,
             frequency = self.frequency
         )
+    
+    # Gets HSV values
+    def get_HSV(self) -> tuple[int]:
+
+        if self.HSV:
+            return self.HSV
+
+        # Maps RGP to the range [0,1]
+        r = self.R / 255
+        g = self.G / 255
+        b = self.B / 255
+
+        # Initialises values
+        h, s, v = None, None, None
+
+        # Used in calculations
+        cmax = max(r, g, b)
+        cmin = min(r, g, b)
+        diff = cmax - cmin
+
+
+        # Finds hue
+        if cmax == cmin:
+            h = 0
+        elif cmax == r:
+            h = (60 * ((g - b) / diff) + 360) % 360
+        elif (cmax == g):
+            h = (60 * ((b - r) / diff) + 120) % 360
+        elif (cmax == b):
+            h = (60 * ((r - g) / diff) + 240) % 360
+        
+        # Finds saturation
+        if cmax == 0:
+            s = 0
+        else:
+            s = diff / cmax * 100
+
+        # Finds value
+        v = cmax * 100
+
+        # Gets values
+        self.H = int(round(h, 0))
+        self.S = int(round(s, 0))
+        self.V = int(round(v, 0))
+        self.HSV = (self.H, self.S, self.V)
+        return self.HSV
+
 
 
 # Returns the average colour in a set of colours
