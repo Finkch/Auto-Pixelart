94ef076873eab990f0456256378a1957918841dc
Finkch <skyler@thealdersons.org>
2024-06-12 13:56:14 -0700

    Added tests for downscaling image before grabbing palette

--- performance_tests/test_colour.py
+++ performance_tests/test_colour.py
@@ -23,6 +23,12 @@ def test_image_RGB_HSV(trials):
 
     return args, [image_RGB, image_HSV], ['RGB', 'HSV'], False
 
+# Tests the effectiveness of downsizing an image before getting its palette
+def test_get_palette(trials):
+    args = []
+
+    return args, [image_palette_raw, image_palette_downscale_1500, image_palette_downscale_100], ['No downscale (3072)', '1500', '100'], False
+
 # For colour variation of RGB vs HSV test
 def colour_RGB(r: int, g: int, b: int) -> Colour:
     return Colour(r, g, b, use_HSV = False)
@@ -40,4 +46,23 @@ def image_RGB() -> Image:
 def image_HSV() -> Image:
     image = Image(HSV = True)
     image.set_file('Nora.jpg')
+    return image
+
+# Downscaling or not for getting image palette
+def image_palette_raw() -> Image:
+    image = Image(HSV = False)
+    image.set_file('Nora.jpg')
+    image.get_palette(16)
+    return image
+
+def image_palette_downscale_1500() -> Image:
+    image = Image(HSV = False)
+    image.set_file('Nora.jpg')
+    image.get_palette(16, width = 1500)
+    return image
+
+def image_palette_downscale_100() -> Image:
+    image = Image(HSV = False)
+    image.set_file('Nora.jpg')
+    image.get_palette(16, width = 100)
     return image
