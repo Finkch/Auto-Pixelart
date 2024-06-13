0ce0464e27bd8415e89fc59ae0a047e08210f135
Finkch <skyler@thealdersons.org>
2024-06-13 15:49:23 -0700

    Added tests for various k-mean variants

--- performance_tests/test_sampling.py
+++ performance_tests/test_sampling.py
@@ -4,6 +4,7 @@ from image import Image
 
 from sampling.nearest_neighbour import nearest_neighbour, nearest_neighbour_pil
 from sampling.bilinear import bilinear, bilinear_pil
+from sampling.kmean import pil_palette, put_palette, paint_palette
 
 # Creates a pair of images to test on
 def setup_test():
@@ -12,12 +13,14 @@ def setup_test():
 
     target = Image()
     target.set_resolution(256, source)
+    target.set_palette_size('d')
     target.set_file(f'performance_test.png', inputs = False)
     target.new()
 
     args = (source, target)
 
     return args
+
 
 
 # Nearest neighbour
@@ -32,3 +35,17 @@ def test_bilinear(trials):
     args = setup_test()
 
     return args, [bilinear, bilinear_pil], ['Serial', 'PIL'], False
+
+
+# Tests the three methods of k-mean
+''' RESULTS
+        pil and put clock in at 0.67s and 0.71s respectively.
+        paint is triple that at 1.85s.
+
+        pil does not offer the ability to select custom palette,
+        so paint is likely the method that will be used.
+'''
+def test_kmeans(trials):
+    args = setup_test()
+
+    return args, [pil_palette, put_palette, paint_palette], ['pil', 'put', 'paint'], False
