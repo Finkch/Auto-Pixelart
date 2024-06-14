c5f5c39757a65ef8a3f61f9d29fb965ae0178951
Finkch <skyler@thealdersons.org>
2024-06-14 12:07:51 -0700

    Added alternative entry point

--- auto.py
+++ auto.py
@@ -3,11 +3,53 @@
 # command line set of arguments.
 
 from sys import argv
+from input import choose_downscale
+from image import Image
+from downscale import downscale
 
-
-# Runs the program
+# Runs the program.
+#   arg[0]: input file name with extensions
+#
+# Optional parameters:
+#   'd': [d]ownscaling algorithm
+#   'w': [w]idth of pixel art image
+#   'p': number of colours in the [p]alette
+#   'u': resolution to [u]pscale to
 def auto(args: list, kwargs: dict) -> None:
-    print(args, kwargs)
+    
+    # Sets defaults and gets optional parameters
+    method = 'k'
+    if 'd' in kwargs:
+        method = kwargs['d']
+
+    width = -1
+    if 'w' in kwargs:
+        width = int(kwargs['w'])
+
+    palette = 8
+    if 'p' in kwargs:
+        palette = kwargs['p']
+
+
+    # Gets the downscaler.
+    #   Defaults to 'k', k-mean clustering
+    downscale_mode, downscale_name = choose_downscale(method)
+
+    # Obtains the source image
+    image = Image(args[0])
+
+    # Downscales the image
+    pixel_art = downscale(image, downscale_mode, downscale_name, width, palette)
+    pixel_art.update()
+
+    # Upscales the image, if specified
+    if 'u' in kwargs:
+        pixel_art.scale(int(kwargs['u']))
+
+    # Saves the image
+    pixel_art.save()
+    print(f'See `outputs/{pixel_art.file}` for your image.\n')
+
 
 
 # Reads argv as a list, grabbing arguments
