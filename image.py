78de95aaac8613981831b49e1e31f0821085ffa0
Finkch <skyler@thealdersons.org>
2024-06-15 13:56:02 -0700

    If size in get_palette is not specified, use Image.palette_size

--- image.py
+++ image.py
@@ -132,7 +132,7 @@ class Image():
     
     # Gets best representation for the image's palette.
     # Based on StackOverflow code: https://stackoverflow.com/questions/3241929/how-to-find-the-dominant-most-common-color-in-an-image
-    def get_palette(self, size: int, top: int = None, width: int = None) -> ndarray[Colour]:
+    def get_palette(self, size: int = None, top: int = None, width: int = None) -> ndarray[Colour]:
         if not top:
             top = size
 
