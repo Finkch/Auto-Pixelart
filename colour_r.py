c8e8d2531e671973d4da822d47b81ba59c02870a
Finkch <skyler@thealdersons.org>
2024-06-18 11:49:10 -0700

    Simplyified ColourList by forcing init input to be PImage colour data

--- colour_r.py
+++ colour_r.py
@@ -1,31 +1,16 @@
 # Contains an object to represent colour
 
-import numpy as np
-from numpy import array, ndarray
-
-import PIL.Image as Pim
+from numpy import ndarray
 
 class ColourList:
-    def __init__(self, source: tuple | Pim.Image) -> None:
-        if isinstance(source, Pim.Image):
-            data = self.colours_from_source(source)
-        else:
-            data = self.colours_from_ndarray
-        
-        self.data           = data[0]
-        self.colours        = data[1]
-        self.frequencies    = data[2]
+    def __init__(self, image_colours: ndarray) -> None:
+        self.data           = image_colours
+        self.colours        = image_colours[:, 1]
+        self.frequencies    = image_colours[:, 0]
 
     # Returns colour data
     def __call__(self) -> ndarray:
         return self.colours
-
-    # Loads colours from a source 
-    def colours_from_source(self, source: Pim.Image) -> tuple:
-        return self.colours_from_ndarray(array(source.getcolors(np.multiply(*source.size))))
-        
-    def colours_from_ndarray(self, data: ndarray) -> tuple:
-        return data, data[:, 1], data[:, 0]
     
     # Given a tuple of colour, returns the most similar
     # that exists in the ColourList
