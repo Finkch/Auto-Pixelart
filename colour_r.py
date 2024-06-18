78c0c35ce311a90f190ffae701efdade572a84d3
Finkch <skyler@thealdersons.org>
2024-06-18 14:24:20 -0700

    Added function to find the difference between two colours

--- colour_r.py
+++ colour_r.py
@@ -43,3 +43,21 @@ def average_colour(colours: ColourList) -> tuple:
             average(colours.colours[:, 2], colours.frequencies),
         )
     )
+
+# Finds the difference between a pair of colours
+def colour_difference_RGB(a: tuple, b: tuple) -> float:
+    return sum([
+        (a[1][0] - b[1][0]) ** 2,
+        (a[1][1] - b[1][1]) ** 2,
+        (a[1][2] - b[1][2]) ** 2
+    ])
+
+def colour_difference_HSV(a: tuple, b: tuple) -> float:
+    return sum([
+        (min( # Hue is circular
+            abs(a[1][0] - b[1][0]), 
+            255 - abs(a[1][0] - b[1][0])
+        ) * 2) ** 2,
+        (a[1][1] - b[1][1]) ** 2,
+        (a[1][2] - b[1][2]) ** 2
+    ])
