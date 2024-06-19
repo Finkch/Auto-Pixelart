fbd3aedc5ea079b948b2af6ab3acc473d6719cc9
Finkch <skyler@thealdersons.org>
2024-06-19 11:58:37 -0700

    Removed sampling directory. No longer needed since funcitonality is now in Image.resize()

--- sampling/sinc.py
+++
@@ -1,8 +0,0 @@
-
-from image import Image
-from PIL.Image import LANCZOS
-
-# Using PIL's resizing method
-def sinc_pil(input: Image, output: Image, log = False):
-    output.source = input.source.resize((output.width, output.height), resample = LANCZOS)
-    return output
